a61a2d23313c140cc22c839311ba21c6779d6dd6
mfilkawi <m.filkawi@gmail.com>
2020-10-16 21:42:31 +0300

    init commit

---
+++ views/javascript/alerts.js
@@ -0,0 +1,12 @@
+var url_string = window.location.href
+var url = new URL(url_string);
+var status = url.searchParams.get("status");
+window.history.replaceState({}, document.title, "/");
+
+if (status !== 'null') {
+    const alert = document.querySelector('.msg');
+    if(status == 'canceled') alert.innerHTML = `<div class="alert danger"><p>لقد لغيت العملية</p></div>`;
+    if(status == 'userexists') alert.innerHTML = `<div class="alert warning"><p>انت مشترك مسبقاً</p></div>`;
+    if(status == 'error') alert.innerHTML = `<div class="alert danger"><p>الرجاء معاودة المحاولة في وقت لاحق</p></div>`;
+    if(status == 'useradded') alert.innerHTML = `<div class="alert success"><p>تم الاشتراك</p></div>`;
+}
