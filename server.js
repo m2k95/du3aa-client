3ab6ab7c8847803382c20dd1c9204064a368d0fe
mfilkawi <m.filkawi@gmail.com>
2020-10-20 22:38:51 +0300

    connection to backend removed

--- server.js
+++ server.js
@@ -6,27 +6,13 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 4000
 
-const API_URL = process.env.API_URL
+// const API_URL = process.env.API_URL
 
 app.use(express.static(path.join(__dirname, "views")));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>{
-    fetch(API_URL + '/twitter/oauth', {method: 'POST'})
-    .then(resp => resp.json())
-    .then(data => {
-        var button
-        if (data.url == undefined){
-            button = '<p style="color: #ff3535">الرجاء المحاولة لاحقاً</p>'
-        }else{
-            button = `<a href="${data.url}" class="btn">اضغط هنا للإشتراك</a>`
-        }
-        res.render('index', {url:button})
-    })
-    .catch(err => {
-        console.log(err)
-        res.render('index', {url:'<p style="color: #ff3535">الرجاء المحاولة لاحقاً</p>'})
-    })
+    res.render('index')
 })
 
 app.get('/prayer', (req, res) => {
