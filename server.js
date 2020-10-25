1c9090989515a93fb16f5b142f7a678787a58ec8
mfilkawi <m.filkawi@gmail.com>
2020-10-25 16:56:28 +0300

    prayer API passed from back-end

--- server.js
+++ server.js
@@ -6,26 +6,58 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 4000
 
-// const API_URL = process.env.API_URL
-
-app.use(express.static(path.join(__dirname, "views")));
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "views")))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) =>{
-    res.render('index')
+    var today = new Date()
+    var dd = String(today.getDate()).padStart(2, '0')
+    var mm = String(today.getMonth() + 1).padStart(2, '0')
+    var yyyy = today.getFullYear()
+    today = dd + '-' + mm + '-' + yyyy
+
+    fetch(`https://api.aladhan.com/timingsByAddress/${today}?address=kuwait,al-asimah&method=9`)
+    .then(resp => resp.json())
+    .then(data => {
+        if (data.code == 200){
+            var Fajr = data.data.timings.Fajr
+            var Dhuhr = data.data.timings.Dhuhr
+            var Asr = data.data.timings.Asr
+            var Maghrib = data.data.timings.Maghrib
+            var Isha = data.data.timings.Isha
+    
+            var prayer = {
+                "Fajr": `${reformatDate(Fajr)}`,
+                "Dhuhr": `${reformatDate(Dhuhr)}`,
+                "Asr": `${reformatDate(Asr)}`,
+                "Maghrib": `${reformatDate(Maghrib)}`,
+                "Isha": `${reformatDate(Isha)}`,
+                "Date": today
+            }
+            res.render('index', {prayer})
+        }else{
+            res.render('index')
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.render('index')
+    })
 })
 
 app.get('/prayer', (req, res) => {
     res.render('prayer')
 })
 
-app.all('*', (req, res) => {
-    res.redirect('/')
-})
-
 app.use((err, req, res, next) => {
     console.error('::: CATCH ERROR ::: ' + err)
     res.send('501 internal error')
 })
+
+function reformatDate(date) {
+	h = date.toString().split(':')[0]
+    m = date.toString().split(':')[1]
+	return parseInt(h) > 12? `${parseInt(h) - 12}:${m} م` : `${parseInt(h)}:${m} ص`
+}
 
 app.listen(PORT)
