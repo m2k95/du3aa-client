7feaf6f919eb092b5010eb3252e3881e52df5304
mfilkawi <m.filkawi@gmail.com>
2020-11-01 19:06:33 +0300

    dark-mode script added

--- views/javascript/dark-mode.js
+++ views/javascript/dark-mode.js
@@ -1,7 +1,14 @@
-// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//     // dark mode
-// }
+const twitter_feed = document.querySelector('.twitter-timeline')
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.classList = ''
+  document.body.classList.toggle('dark')
+  twitter_feed.setAttribute('data-theme', 'dark')
+}
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-    const newColorScheme = e.matches ? "dark" : "light";
-});
+  const newColorScheme = e.matches ? 'dark' : 'light'
+  document.body.classList = ''
+  document.body.classList.toggle(newColorScheme)
+  twitter_feed.setAttribute('data-theme', newColorScheme)
+})
