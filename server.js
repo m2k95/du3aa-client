a9ca96767bb21ec57633a31993fc9c09773530f4
mymk95 <m.filkawi@gmail.com>
2021-01-12 21:41:03 +0300

    fetch prayer removed

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const express = require('express')
-const fetch = require('node-fetch')
 const path = require('path')
 
 const app = express()
@@ -12,51 +11,13 @@ app.use(express.static(path.join(__dirname, 'views')))
 app.set('view engine', 'ejs')
 
 app.get('*', (req, res) => {
-  let today = new Date()
-  const dd = String(today.getDate()).padStart(2, '0')
-  const mm = String(today.getMonth() + 1).padStart(2, '0')
-  const yyyy = today.getFullYear()
-  today = dd + '-' + mm + '-' + yyyy
-
-  fetch(`https://api.aladhan.com/timingsByAddress/${today}?address=kuwait,al-asimah&method=9`)
-    .then(resp => resp.json())
-    .then(data => {
-      if (data.code === 200) {
-        const Fajr = data.data.timings.Fajr
-        const Dhuhr = data.data.timings.Dhuhr
-        const Asr = data.data.timings.Asr
-        const Maghrib = data.data.timings.Maghrib
-        const Isha = data.data.timings.Isha
-
-        const prayer = {
-          Fajr: `${reformatDate(Fajr)}`,
-          Dhuhr: `${reformatDate(Dhuhr)}`,
-          Asr: `${reformatDate(Asr)}`,
-          Maghrib: `${reformatDate(Maghrib)}`,
-          Isha: `${reformatDate(Isha)}`,
-          Date: today
-        }
-        res.render('index', { prayer, version: pkg.version })
-      } else {
-        res.render('index', { version: pkg.version })
-      }
-    })
-    .catch(err => {
-      console.log(err)
-      res.render('index')
-    })
+  return res.render('index', { version: pkg.version })
 })
 
 app.use((err, req, res, next) => {
   console.error('::: CATCH ERROR ::: ' + err)
   res.status(501)
-  res.send('501 internal error')
+  return res.send('Internal Server Error')
 })
-
-function reformatDate (date) {
-  const h = date.toString().split(':')[0]
-  const m = date.toString().split(':')[1]
-  return parseInt(h) > 12 ? `${parseInt(h) - 12}:${m} م` : `${parseInt(h)}:${m} ص`
-}
 
 app.listen(PORT)
